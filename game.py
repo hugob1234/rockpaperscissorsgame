40cb0ca7010ef5920d4ac7adc7d003392fa20dc8
hugob1234 <159055356+hugob1234@users.noreply.github.com>
2024-02-12 19:44:15 +0000

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,17 @@
 import random
 
-user = input("Enter a choice (rock, paper, scissors): ")
-computer=random.choice(["rock", "paper", "scissors"])
+while True:
+  user = input("Enter a choice (rock, paper, scissors): ")
+  computer=random.choice(["rock", "paper", "scissors"])
 
 
-if user == computer:
-  print("Its a tie!")
-elif computer == "rock" and user == "scissors":
-  print("Computer wins!")
-elif computer == "paper" and user == "rock":
-  print("Computer wins!")
-elif computer == "scissors" and user == "paper":
-  print("Computer wins!")
-else:
-  print("You win!!!")
+  if user == computer:
+    print("Its a tie!")
+  elif computer == "rock" and user == "scissors":
+    print("Computer wins!")
+  elif computer == "paper" and user == "rock":
+    print("Computer wins!")
+  elif computer == "scissors" and user == "paper":
+    print("Computer wins!")
+  else:
+    print("You win!!!")
